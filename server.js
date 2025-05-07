d390078d792bba09c9d077dc8589686655265b89
bot19 <jsbot@pm.me>
2025-05-07 10:45:31 +1000

    handles diff end points, prep for word generation

--- server.js
+++ server.js
@@ -1,35 +1,45 @@
 import express from "express";
 import { generateCharacters } from "./generateChars.js";
+import { generateWords } from "./generateWords.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 let server;
 
-app.get("/naruto", async (req, res) => {
-  const namesParam = req.query.names;
+const handleRequest = async (req, res, generator) => {
+  const listParam = req.query.list;
 
   // error handling: missing query parameters
-  if (!namesParam) {
-    return res.status(400).send('Missing "names" query parameter.');
+  if (!listParam) {
+    return res.status(400).send('Missing "list" query parameter.');
   }
 
-  const names = namesParam.split(",").map((name) => name.trim());
+  const list = listParam.split(",").map((name) => name.trim());
 
   try {
-    // const results = await Promise.all(names.map(asyncTask));
-    console.log("Names to process:", names);
+    // const results = await Promise.all(list.map(asyncTask));
+    console.log("List to process:", list, `\n---`);
 
-    generateCharacters(names).then((characters) => {
-      // TODO: this log outputs weird data sometimes, no idea why
-      console.log("Results:", JSON.stringify(characters, null, 2));
-      res.json(characters); // responds with a JSON array = works GOOD
-    });
+    const result = await generator(list);
+
+    // TODO: this log outputs weird data sometimes, no idea why
+    console.log("Results:", JSON.stringify(result, null, 2));
+
+    res.json(result); // responds with a JSON array = works GOOD
   } catch (error) {
     // error handling: async task failure
-    console.error("Error processing names:", error);
+    console.error("Error processing list:", error);
     res.status(500).send("Server error");
   }
+};
+
+app.get("/naruto", async (req, res) => {
+  return await handleRequest(req, res, generateCharacters);
+});
+
+app.get("/words", async (req, res) => {
+  return await handleRequest(req, res, generateWords);
 });
 
 // Assign the server instance
