ad26496491fddef8293e5297ee45b147ab7bdb21
bot19 <jsbot@pm.me>
2025-05-19 23:18:19 +1000

    results data 2

---
+++ output-chars/2-Fil.js
@@ -0,0 +1,19 @@
+const output = [
+  {
+    name: "Fil",
+    nickname: "Shadow Mirage",
+    rank: "Genin",
+    village: '"Village Hidden in the Leaves, Konohagakure"',
+    natureAffinity: ["Shadow", "Wind"],
+    uniqueAbilities: [
+      "Shadow Clone Technique: Creates copies of the user that can act independently.",
+      "Illusory Mirage: Creates realistic illusions to confuse opponents and manipulate their perception of reality.",
+    ],
+    feats: [
+      "1. Successfully infiltrated an enemy stronghold undetected.",
+      "2. Executed a high-level Shadow Clone Technique to distract multiple opponents.",
+      "3. Rescued a fellow shinobi during a high-stakes mission using stealth and agility.",
+      "4. Analyzed and countered a Genjutsu during a Chunin Exam match.",
+    ],
+  },
+];
